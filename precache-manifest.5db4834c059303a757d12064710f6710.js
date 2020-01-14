ae6e415dfaa5a2e35bdae1eca046670f5d737852
manny i. agbayani <serverless.yoda@gmail.com>
2020-01-14 17:55:00 +1300

    Updates

---
+++ precache-manifest.5db4834c059303a757d12064710f6710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff49035e37bcd332361b71bbac813d5d",
+    "url": "/monsters-friend/index.html"
+  },
+  {
+    "revision": "972dba97473b35d7b93c",
+    "url": "/monsters-friend/static/css/main.fca36ca6.chunk.css"
+  },
+  {
+    "revision": "eb8fa2d92c56d9766aaf",
+    "url": "/monsters-friend/static/js/2.c929b1ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-friend/static/js/2.c929b1ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "972dba97473b35d7b93c",
+    "url": "/monsters-friend/static/js/main.5f875c86.chunk.js"
+  },
+  {
+    "revision": "8dc9f2e83acf14815cd4",
+    "url": "/monsters-friend/static/js/runtime-main.169fcd5c.js"
+  }
+]);
